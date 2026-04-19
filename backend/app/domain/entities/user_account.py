7650e3ff44c71824e1f29ce5cb9fbf3feb80efe9
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-04-19 01:39:16 -0300

    feat(UserAccount): Implementar entidade conta de usuário

---
+++ backend/app/domain/entities/user_account.py
@@ -0,0 +1,83 @@
+class UserAccount:
+
+    """Representa a entidade conta de usuário da regra de negócio"""
+
+    def __init__(self, name: str, email: str, password: str, phone: str) -> None:
+
+        """Inicializa os atributos de instância de UserAccount
+
+        Parameters
+        ----------
+        name: str
+            Nome completo do usuário
+        
+        email: str
+            E-mail do usuário
+        
+        password: str
+            Senha do usuário
+        
+        phone: str
+            Telefone do usuário
+        """
+
+        self.__full_name = name
+        self.__email = email
+        self.__password = password
+        self.__phone = phone
+    
+    @property
+    def name(self) -> str:
+
+        """Obtém o nome completo do usuário
+
+        Returns
+        -------
+        str
+            Nome completo do usuário
+        
+        """
+
+        return self.__full_name
+
+    @property
+    def email(self) -> str:
+
+        """Obtém o e-mail do usuário
+
+        Returns
+        -------
+        str
+            E-mail do usuário
+        
+        """
+
+        return self.__email
+
+    @property
+    def password(self) -> str:
+
+        """Obtém a senha do usuário
+
+        Returns
+        -------
+        str
+            Senha do usuário
+        
+        """
+
+        return self.__password
+
+    @property
+    def phone(self) -> str:
+
+        """Obtém o telefone do usuário
+
+        Returns
+        -------
+        str
+            telefone do usuário
+        
+        """
+
+        return self.__phone
