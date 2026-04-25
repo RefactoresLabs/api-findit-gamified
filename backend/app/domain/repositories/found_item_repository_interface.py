df480e17f7683f736fc7c0a860acac8a36cae257
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-04-25 19:13:46 -0300

    feat(FoundItemRepositoryInterface): Implementa abstração do repositório de itens encontrados

---
+++ backend/app/domain/repositories/found_item_repository_interface.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+from backend.app.domain.entities.found_item import FoundItem
+
+
+class FoundItemRepositoryInterface(ABC):
+
+    @abstractmethod
+    def create_new_found_item(self, found_item: FoundItem) -> FoundItem: ...
+
+    @abstractmethod
+    def update_found_item(self, found_item: FoundItem, id: int) -> FoundItem: ...
+
+    @abstractmethod
+    def get_found_item_by_id(self, id: int) -> FoundItem: ...
+
+    @abstractmethod
+    def delete_found_item(self, id: int) -> None: ...
